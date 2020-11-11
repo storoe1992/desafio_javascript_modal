486d20fbd7390dcacba12e30ae24521dca320e55
Sergio Toro Escofet <storoe1992@gmail.com>
2020-11-11 10:56:35 -0300

    1er commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    $("a").click(function(e){
+        if(this.hash !== ""){
+            e.preventDefault();
+            var gato = this.hash;
+            $('html, body').animate({
+                scrollTop : $(gato).offset().top
+            },800,function(){
+                window.location.hash = gato;
+            });
+        }
+    });
+
+    $('[data-toggle="popover"]').popover();
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.example-popover').popover({
+        container: 'body'
+      })
+});
